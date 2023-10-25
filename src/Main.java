4a076359d4ba226fee9e4001ab301b3d4f4df424
Vasilek54 <sonya_monya17@ukr.net>
2023-10-25 21:50:52 +0300

    Added class Book

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Book book = new Book("Lord of the rings", "Tolkien", "3213123", 1954);
+        System.out.println(book.getAuthor());
+        System.out.println(book.getIsbn());
+        System.out.println(book.getPublicationYear());
+        System.out.println(book.getTitle());
+        book.setTitle("Lord of the rings 1");
+        System.out.println(book);
     }
 }
